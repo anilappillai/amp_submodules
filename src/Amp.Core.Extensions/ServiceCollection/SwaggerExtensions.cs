1945aa2548b77501f7e0ba07e5f6b129cc546afa
anilappillai <anilappillai@gmail.com>
2026-04-06 19:09:10 -0400

    git files

--- src/Amp.Core.Extensions/ServiceCollection/SwaggerExtensions.cs
+++ src/Amp.Core.Extensions/ServiceCollection/SwaggerExtensions.cs
@@ -1,5 +1,7 @@
 using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -50,6 +52,25 @@ public static class SwaggerExtensions
         });
 
         return services;
+    }
+
+    /// <summary>
+    /// Enables Swagger and SwaggerUI (with versioned endpoints) in Development.
+    /// Usage: app.UseAmpCoreSwagger();
+    /// </summary>
+    public static IApplicationBuilder UseAmpCoreSwagger(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+            return app;
+
+        app.UseSwagger();
+        app.UseSwaggerUI(opts =>
+        {
+            foreach (var desc in app.DescribeApiVersions())
+                opts.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json", desc.GroupName);
+        });
+
+        return app;
     }
 }
 
